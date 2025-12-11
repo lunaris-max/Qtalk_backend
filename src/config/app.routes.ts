adaa1f93a50163d3102fcb345cc474ec21ac414d
lunaris <LunarisMax@proton.me>
2025-12-11 12:54:04 +0200

    feat: add router config

---
+++ src/config/app.routes.ts
@@ -0,0 +1,15 @@
+// Root
+const usersRoot = 'users';
+
+// Api Versions
+const v1 = 'v1';
+
+export const routesV1 = {
+  version: v1,
+  user: {
+    root: usersRoot,
+    delete: `/${usersRoot}/:id`,
+    findOne: `/${usersRoot}/:id`,
+    update: `/${usersRoot}/:id`,
+  },
+};
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,33 +1,42 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { routesV1 } from 'src/config/app.routes';
 
-@Controller('users')
+@Controller(routesV1.version)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
+  @Post(routesV1.user.root)
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
-  @Get()
+  @Get(routesV1.user.root)
   findAll() {
     return this.usersService.findAll();
   }
 
-  @Get(':id')
+  @Get(routesV1.user.findOne)
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Patch(routesV1.user.update)
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
-  @Delete(':id')
+  @Delete(routesV1.user.delete)
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
