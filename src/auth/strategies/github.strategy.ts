ff814bd537c17010a5edcbe8a9c1d08b9cc127fd
lunaris <LunarisMax@proton.me>
2026-01-06 10:47:16 +0200

    fix: upd dependence

--- src/auth/strategies/github.strategy.ts
+++ src/auth/strategies/github.strategy.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { AuthProvider } from '@prisma/client';
-import { Strategy, Profile } from 'passport-google-oauth20';
+import { Strategy } from 'passport-github2';
 @Injectable()
 export class GithubStrategy extends PassportStrategy(Strategy, 'github') {
   constructor(config: ConfigService) {
     super({
       clientID: config.get('GITHUB_CLIENT_ID'),
       clientSecret: config.get('GITHUB_CLIENT_SECRET'),
-      callbackURL: 'https://teamchallenge-chat-backend.onrender.com/v1/auth/github',
+      callbackURL: 'https://teamchallenge-chat-backend.onrender.com/v1/auth/github/callback',
       scope: ['user:email'],
     });
   }
